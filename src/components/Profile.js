d6fee4de1f3aa77e4770704834288a3b281b401c
Dan Garro <dobetterwithdan@gmail.com>
2023-03-24 18:49:49 -0400

    updated Skills component to include modal for adding/updating skill

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,67 @@
-// Display User Profile at top of page 
+// Display User Profile at top of page
 
-import { Container, Row, Col } from 'react-bootstrap'; 
+import { Container, Row, Col, Button, Form, Modal } from 'react-bootstrap'; 
 import API from '../API';
 import { useState, useEffect } from 'react'; 
 
 
 const Profile = () => {
     const [profile, setProfile] = useState(""); 
-    
+    const [title, setTitle] = useState(""); 
+    const [description, setDescription] = useState(""); 
+    const [profileId, setProfileId] = useState(null); 
+    const [profiles, setProfiles] = useState([]); 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => {
+        setShow(false);
+        setTitle("");
+        setDescription("");
+    };
+
+    const handleShow = () => setShow(true); 
+
+    useEffect(() => {
+        refreshProfiles();
+    }, []); 
+
+    const refreshProfiles = () => {
+        API.get("profiles/")
+            .then((res) => {
+                setProfiles(res.data); 
+                // setTitle(res[0].title);
+                // setDescription(res[0].description);
+                // setProfileId(res[0].id);
+            })
+            .catch(console.error);
+    };
+
+        const saveProfile = (e) => {
+            e.preventDefault(); 
+            let item = { title, description };
+            API.post("profiles/", item).then(() => refreshProfiles()); 
+            setTitle("");
+            setDescription("");
+        };
+
+        const onUpdate = (id) => {
+            let item = { title, description }; 
+            API.put(`profiles/${id}/`, item).then((res) => refreshProfiles());
+            setTitle("");
+            setDescription(""); 
+        };
+    /*
+        const onDelete = (id) => {
+            API.delete(`profiles/${id}/`).then((res) => refreshProfiles());
+        };
+
+        function selectProfile(id) {
+            let item = profiles.filter((profile) => profile.id === id)[0];
+            setTitle(item.title);
+            setDescription(item.description); 
+            setProfileId(item.id); 
+        }
+    */
 
     useEffect(() => {
         getProfile();
@@ -34,6 +88,64 @@ const Profile = () => {
                         {profile.description}
                     </p>
                 </Col>
+            </Row>
+            <Row>
+                <div className="col-md-4">
+                    <Button variant="outline-secondary" onClick={handleShow}>
+                        Create/Update Profile
+                    </Button>
+
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Profile</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form onSubmit={saveProfile} className="mt-4">
+                                <Form.Group className="mb-3" controlId="formBasicTitle">
+                                    <Form.Label>{profileId} Title</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Enter Title"
+                                        value={title}
+                                        onChange={(e) => setTitle(e.target.value)}
+                                    />
+                                </Form.Group>
+
+                                <Form.Group className="mb-3" controlId="formBasicDescription">
+                                    <Form.Label>Description</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        as="textarea"
+                                        rows={6}
+                                        placeholder="Enter Description"
+                                        value={description}
+                                        onChange={(e) => setDescription(e.target.value)}
+                                    />
+                                </Form.Group>
+
+                                <div className="float-right">
+                                    <Button 
+                                        variant="outline-success"
+                                        type="submit"
+                                        onClick={saveProfile}
+                                        className="mx-2"
+                                    >Save</Button>
+                                    <Button 
+                                        variant="outline-primary"
+                                        type="button"
+                                        onClick={() => onUpdate(profileId)}
+                                        className="mx-2"
+                                    >Update</Button>
+                                </div>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="outline-danger" onClick={handleClose}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </div>
             </Row>
         </Container>
     )
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,21 +1,67 @@
-import { Container, Card } from 'react-bootstrap'; 
+import { Container, Card, Button, Form, Modal, ListGroup } from 'react-bootstrap'; 
 import API from '../API';
 import { useState, useEffect } from 'react'; 
 
+
+
 const Skills = () => {
     const [skills, setSkills] = useState([]);
+    const [name, setName] = useState("");
+    const [level, setLevel] = useState("");
+    const [type, setType] = useState("");
+    const [skillId, setSkillId] = useState(null);
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => {
+        setShow(false);
+        setName("");
+        setLevel("");
+        setType(""); 
+    };
+
+    const handleShow = () => setShow(true); 
 
     useEffect(() => {
-        getSkills();
+        refreshSkills();
     }, []);
 
-    const getSkills = () => {
+    const refreshSkills = () => {
         API.get("skills/")
             .then((res) => {
                 setSkills(res.data);
             })
             .catch(console.error);
     };
+
+    
+    const onSubmit = (e) => {
+        e.preventDefault();
+        let item = { name, level, type };
+        API.post("skills/", item).then(() => refreshSkills()); 
+        setName("");
+        setLevel("");
+        setType(""); 
+    };
+
+    const onUpdate = (id) => {
+        let item = { name, level, type };
+        API.put(`skills/${id}/`, item).then((res) => refreshSkills());
+        setName("");
+        setLevel("");
+        setType(""); 
+    };
+
+    const onDelete = (id) => {
+        API.delete(`skills/${id}/`).then((res) => refreshSkills());
+    }; 
+
+    function selectSkill(id) {
+        let item = skills.filter((skill) => skill.id === id)[0];
+        setName(item.name);
+        setLevel(item.level);
+        setType(item.type);
+        setSkillId(item.id); 
+    }
 
     return (
         <Container className="w-75 mt-3" id="skills-container">
@@ -34,14 +80,112 @@ const Skills = () => {
                                         {skill.name}
                                     </Card.Title>
                                     <Card.Text>
-                                        Level: {skill.level}
+                                        <ListGroup>
+                                            <ListGroup.Item>
+                                                Level: {skill.level}
+                                            </ListGroup.Item>
+                                            <ListGroup.Item>
+                                                Type: {skill.type}
+                                            </ListGroup.Item>
+                                        </ListGroup>
                                     </Card.Text>
                                 </Card.Body>
-                                <Card.Footer>Type: {skill.type}</Card.Footer>
+                                <Card.Footer>
+                                    <div className="float-right">
+                                        <Button 
+                                            variant="outline-secondary"
+                                            size="sm"
+                                            type="button"
+                                            onClick={() => selectSkill(skill.id)}
+                                            className="d-inline m-2"
+                                        >
+                                            Select
+                                        </Button>
+                                        <Button 
+                                            variant="outline-danger"
+                                            size="sm"
+                                            type="button"
+                                            onClick={() => onDelete(skill.id)}
+                                            className="d-inline m-2"
+                                        >
+                                            Delete
+                                        </Button>
+                                    </div>
+                                </Card.Footer>
                             </Card>
                         </div>
                     )
                 })}
+            </div>
+            <div className="row mt-3">
+                <div className="col-md-4">
+                    <Button variant="outline-secondary" onClick={handleShow}>
+                        Create/Update Skill
+                    </Button>
+
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Skill</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <form id="formSkill" onSubmit={onSubmit} className="mt-4">
+                                <Form.Group className="mb-3" controlId="formBasicName">
+                                    <Form.Label>{skillId} Name</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Enter Skill Name"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                    />
+                                </Form.Group>
+
+                                <Form.Group className="mb-3" controlId="formBasicLevel">
+                                    <Form.Select 
+                                        id="selectLevel"
+                                        value={level}
+                                        onChange={(e) => setLevel(e.target.value)}
+                                    >
+                                        <option>Skill Level</option>
+                                        <option value="advanced">advanced</option>
+                                        <option value="familiar">familiar</option>
+                                    </Form.Select>
+                                </Form.Group>
+
+                                <Form.Group className="mb-3" controlId="formBasicType">
+                                    <Form.Select 
+                                        id="selectType"
+                                        value={type}
+                                        onChange={(e) => setType(e.target.value)} 
+                                    >
+                                        <option>Skill Type</option>
+                                        <option value="hard">hard</option>
+                                        <option value="soft">soft</option>
+                                    </Form.Select>
+                                </Form.Group>
+
+                                <div className="float-right">
+                                    <Button 
+                                        variant="outline-success"
+                                        type="submit"
+                                        onClick={onSubmit}
+                                        className="mx-2"
+                                    >Save</Button>
+                                    <Button 
+                                        variant="outline-primary"
+                                        type="button"
+                                        onClick={() => onUpdate(skillId)}
+                                        className="mx-2"
+                                    >Update</Button>
+                                </div>
+                            </form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="outline-danger" onClick={handleClose}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </div>
             </div>
         </Container>
     )
